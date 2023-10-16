3cdc58a35f30b686baa81e3b872eba712770ffdc
SidVeld <sidveld@gmail.com>
2023-10-16 23:00:46 +0300

    Add `Grudge` extension

--- bot/models/user.py
+++ bot/models/user.py
@@ -3,10 +3,9 @@ from tortoise.models import Model
 
 
 class UserModel(Model):
-    user_id = fields.IntField(pk=True, unique=True)
-    discord_id = fields.BigIntField(unique=True)
+    user_id = fields.BigIntField(pk=True, unique=True)
     username = fields.TextField()
-    registered_at = fields.DatetimeField(null=True, auto_now_add=True)
+    added_at = fields.DatetimeField(auto_now_add=True)
 
     class Meta:
-        table = "user"
+        table = "discord_user"
