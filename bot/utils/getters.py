5823bcee5a27a274d09c3ab6fbb37dbe1ca7c8fc
SidVeld <sidveld@gmail.com>
2023-10-23 23:09:45 +0300

    Add `get_version` getter util

---
+++ bot/utils/getters.py
@@ -0,0 +1,20 @@
+import random
+from pathlib import Path
+from tomllib import load
+
+
+PYPROJECT_PATH = Path("pyproject.toml")
+
+
+def get_version() -> str:
+    pyproject_data = load(PYPROJECT_PATH.open("rb"))
+    return pyproject_data["tool"]["poetry"]["version"]
+
+
+def get_quote() -> str:
+    quotes = [
+        "Young and stupid.",
+        "From dirt to dukes.",
+        "I love you, I hate you."
+    ]
+    return random.choice(quotes)
