e849636ebf95ce2070c3b6f95667a25aa88a5f15
SidVeld <sidveld@gmail.com>
2023-11-16 12:56:25 +0300

    refactor: :recycle: add config functions docstring

--- bot/config.py
+++ bot/config.py
@@ -3,10 +3,20 @@ from dataclasses import dataclass
 
 from dotenv import load_dotenv
 
-from .classes.exceptions import UnconvertibleVariableError
+from .classes.exceptions import InconvertibleVariableError
 
 
 def get_bool(env_value: str) -> bool:
+    """
+    Converts the type of the received environment variable to bool.
+
+    The variable must be either `True` or `False`.
+    Otherwise, an error will appear.
+
+    :param str env_value: The value of the environment variable to be converted. Case-insensitive.
+    :return: Value of environment variable of `bool` type.
+    :raises InconvertibleVariableError: A variable cannot be converted to a `bool`.
+    """
     match env_value.lower():
         case "true":
             return True
@@ -14,11 +24,17 @@ def get_bool(env_value: str) -> bool:
             return False
         case _:
             message = f"Cannot convert '{env_value}' to True or False."
-            raise UnconvertibleVariableError(message)
+            raise InconvertibleVariableError(message)
 
 
-def get_list(env_value: str) -> list:
-    return [item for item in env_value.split(",")]
+def get_list(env_value: str) -> list[int]:
+    """
+    Converts the type of the received environment variable to list of integers.
+
+    :param str env_value: The value of the environment variable to be converted.
+    :return: Value of environment variable of `list` type.
+    """
+    return [int(item.strip()) for item in env_value.split(",")]
 
 
 @dataclass
