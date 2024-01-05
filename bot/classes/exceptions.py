a567910388c5658e58b67f49d8dda7cea596e463
SidVeld <sidveld@gmail.com>
2024-01-05 13:25:12 +0300

    feat: :sparkles: add `NoneTypeVariableError`

--- bot/classes/exceptions.py
+++ bot/classes/exceptions.py
@@ -2,3 +2,8 @@ class InconvertibleVariableError(Exception):
     """
     The InconvertibleVariableError exception occurs when a variable type conversion attempt fails.
     """
+
+class NoneTypeVariableError(Exception):
+    """
+    The InconvertibleVariableError exception occurs when a variable does not exist or has no value.
+    """
